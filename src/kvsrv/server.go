4d1f71c2c2b75c939dada39c3b05fb69226fa2db
LaUrrego <lurrego22@gmail.com>
2025-01-20 19:32:47 +0100

    Finalize duplicate checks and reduce memory usage

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -21,57 +21,56 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-type ClientRecord struct {
-	LastSeq   int64
-	LastValue string
-}
-
 type KVServer struct {
 	mu    sync.Mutex
 	store map[string]string
-	// keep track of clients and current sequence number
-	clients map[int64]int64
 	// keep track of clients and last reply sent
-	clientMap map[int64]*ClientRecord
+	clientMap     map[int64]int64
+	appendRecords map[int64]string
 }
 
 func (kv *KVServer) isDuplicate(client int64, seq int64) bool {
-	if val, ok := kv.clients[client]; ok {
-		return val >= seq
+	if lastSeq, ok := kv.clientMap[client]; ok {
+		if lastSeq < seq {
+			delete(kv.clientMap, client)
+			return false
+		} else if lastSeq >= seq {
+			return true
+		}
 	}
 	return false
+}
+
+func (kv *KVServer) checkStore(key string) string {
+	if val, ok := kv.store[key]; ok {
+		return val
+	}
+	return ""
 }
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
-	if dupe := kv.isDuplicate(args.ClientId, args.Seq); dupe {
-		previousReply := kv.clientMap[args.ClientId].LastValue
-		reply.Value = previousReply
-		return
-	}
-	if val, ok := kv.store[args.Key]; !ok {
-		reply.Value = ""
-	} else {
-		reply.Value = val
-	}
+	if dupe := kv.isDuplicate(args.ClientId, args.Seq); !dupe {
+		kv.clientMap[args.ClientId] = args.Seq
 
-	kv.clientMap[args.ClientId] = &ClientRecord{LastSeq: args.Seq, LastValue: reply.Value}
-	kv.clients[args.ClientId] = args.Seq
+	}
+	reply.Value = kv.checkStore(args.Key)
+
+	delete(kv.appendRecords, args.ClientId)
+
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
-	if dupe := kv.isDuplicate(args.ClientId, args.Seq); dupe {
-		previousReply := kv.clientMap[args.ClientId].LastValue
-		reply.Value = previousReply
-		return
+	if dupe := kv.isDuplicate(args.ClientId, args.Seq); !dupe {
+		kv.clientMap[args.ClientId] = args.Seq
+		kv.store[args.Key] = args.Value
 	}
-	kv.store[args.Key] = args.Value
 	reply.Value = kv.store[args.Key]
-	kv.clients[args.ClientId] = args.Seq
-	kv.clientMap[args.ClientId] = &ClientRecord{LastSeq: args.Seq, LastValue: reply.Value}
+
+	delete(kv.appendRecords, args.ClientId)
 
 }
 
@@ -80,27 +79,23 @@ func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	defer kv.mu.Unlock()
 
 	if dupe := kv.isDuplicate(args.ClientId, args.Seq); dupe {
-		previousReply := kv.clientMap[args.ClientId].LastValue
-		reply.Value = previousReply
+		reply.Value = kv.appendRecords[args.ClientId]
 		return
 	}
 	old := kv.store[args.Key]
-	if _, ok := kv.store[args.Key]; !ok {
-		kv.store[args.Key] = args.Value
-	} else {
-		kv.store[args.Key] = old + args.Value
-	}
 	reply.Value = old
-	kv.clients[args.ClientId] = args.Seq
-	kv.clientMap[args.ClientId] = &ClientRecord{LastSeq: args.Seq, LastValue: reply.Value}
+	kv.appendRecords[args.ClientId] = old
+
+	kv.store[args.Key] = old + args.Value
+	kv.clientMap[args.ClientId] = args.Seq
 
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
 	kv.store = make(map[string]string)
-	kv.clients = make(map[int64]int64)
-	kv.clientMap = make(map[int64]*ClientRecord)
+	kv.clientMap = make(map[int64]int64)
+	kv.appendRecords = make(map[int64]string)
 
 	return kv
 }
